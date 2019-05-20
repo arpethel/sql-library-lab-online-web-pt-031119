981ec397b7fc16bb14862894e32b3c3319d5ecd1
Ryan Pethel <arpethel@gmail.com>
2019-05-20 21:36:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS count_species FROM characters
   GROUP BY characters.species
-  ORDER BY count_species ASC"
+  ORDER BY count_species DESC"
 end
 
 def select_name_and_series_subgenres_of_authors
