b3d68309c25f928b6aa940b5bb418c0babe7a9dd
Ryan Pethel <arpethel@gmail.com>
2019-05-20 22:04:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species FROM series
-
+  JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human'"
 end
 
