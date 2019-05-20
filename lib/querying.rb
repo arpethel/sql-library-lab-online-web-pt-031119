5f6e7330a6f8bffd4febc59690e5398bac121f01
Ryan Pethel <arpethel@gmail.com>
2019-05-20 21:43:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  ""
+  "SELECT authors.name, series.subgenre_id FROM authors
+  JOIN series ON series.subgenre_id = subgenres.id
+  JOIN series ON series.author_id = authors.id"
 end
 
 def select_series_title_with_most_human_characters
