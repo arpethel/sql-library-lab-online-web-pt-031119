709cf60c38eca03e1b984f0014a3f5a0d9870bb9
Ryan Pethel <arpethel@gmail.com>
2019-05-20 22:08:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
   COUNT(characters.species) AS count_species
-  
+
   GROUP BY series.title"
 
 end
